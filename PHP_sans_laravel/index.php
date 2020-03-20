b168e34222a36a6aab441e5da1c676254288d436
Noé AMIOT <amiot.noe@icloud.com>
2020-03-20 15:41:50 +0100

    linting ...

--- PHP_sans_laravel/index.php
+++ PHP_sans_laravel/index.php
@@ -1,36 +1,36 @@
 <?php
-  require_once("config.php");
-  //Eviter de mélanger php et html, là c'est pour l'exemple ...
+require_once("config.php");
+//Eviter de mélanger php et html, là c'est pour l'exemple ...
 ?>
 <html>
-  <head>
-    <title>Exemple Oauth2 - portaild des assos</title>
-  </head>
-  <body>
-    <?php
-      if(isset($accessToken)&& !empty($accessToken)) {
-        //getRessourceOwner renvoie le détail de l'utilisateur qui est connecté (var_dump($resourceOwner) pour afficher tout le détail de l'utilisateur)
-        $resourceOwner = $provider->getResourceOwner($accessToken)->toArray();
-        echo "Utilisateur connecté: id : " . $resourceOwner["id"] . ", nom : " . $resourceOwner["name"] . "<br>";
+<head>
+  <title>Exemple Oauth2 - portaild des assos</title>
+</head>
+<body>
+  <?php
+  if(isset($accessToken)&& !empty($accessToken)) {
+    //getRessourceOwner renvoie le détail de l'utilisateur qui est connecté (var_dump($resourceOwner) pour afficher tout le détail de l'utilisateur)
+    $resourceOwner = $provider->getResourceOwner($accessToken)->toArray();
+    echo "Utilisateur connecté: id : " . $resourceOwner["id"] . ", nom : " . $resourceOwner["name"] . "<br>";
 
-        //Pour appeler le serveur des assos, il faut créer une nouvelle requête, ici, on demande les informations sur un utilisateur (les scopes demandés par le client doivent par exemple contenir au moins user-get-email)
-        $request = $provider->getAuthenticatedRequest(
-          'GET', //Protocol
-          'http://localhost:8000/api/v1/users/' . $resourceOwner["id"] . '/assos/', //Url à appeler (ici on demande la liste des associations de l'utilisateur)
-          $accessToken //l'access token
-          //["foo" => "bar"], les paramètres pour la requête
-        );
-        $response = $provider->getParsedResponse($request); //Lancer la requête
-        for($i = 0; $i<count($response); $i++) {
-		echo "<p>asso: id : " . $response[$i]["id"] . ", nom : " . $response[$i]["shortname"] . ", rôle de l'utilisateur : " . $response[$i]["pivot"]["role_id"] . "</p>";
-      	}
-      }
-      else {
-        ?>
-          <p>Utilisateur déconnecté</p>
-          <a href="login.php">Se connecter</a>
-        <?php
-      }
+    //Pour appeler le serveur des assos, il faut créer une nouvelle requête, ici, on demande les informations sur un utilisateur (les scopes demandés par le client doivent par exemple contenir au moins user-get-email)
+    $request = $provider->getAuthenticatedRequest(
+      'GET', //Protocol
+      'http://localhost:8000/api/v1/users/' . $resourceOwner["id"] . '/assos/', //Url à appeler (ici on demande la liste des associations de l'utilisateur)
+      $accessToken //l'access token
+      //["foo" => "bar"], les paramètres pour la requête
+    );
+    $response = $provider->getParsedResponse($request); //Lancer la requête
+    for($i = 0; $i<count($response); $i++) {
+      echo "<p>asso: id : " . $response[$i]["id"] . ", nom : " . $response[$i]["shortname"] . ", rôle de l'utilisateur : " . $response[$i]["pivot"]["role_id"] . "</p>";
+    }
+  }
+  else {
     ?>
-  </body>
+    <p>Utilisateur déconnecté</p>
+    <a href="login.php">Se connecter</a>
+    <?php
+  }
+  ?>
+</body>
 </html>
--- PHP_sans_laravel/retour_oauth2.php
+++ PHP_sans_laravel/retour_oauth2.php
@@ -1,46 +1,45 @@
 <?php
-  /* Cette page est appellée lorsque l'utilisateur vient d'autoriser votre apllication sur le serveur des assos
-  * Si il refuse d'autoriser votre application il n'arrivera pas sur cette page.
-  * Cette page permet de convertir le code renvoyé par oauth2 en access token, qui devra être renseigné pour chaque requête sur le portail
-  * Le seul élément à sauvegarder est l'access token. On le sauvegarde dans la session de l'utilisateur et non dans ses cookies car c'est le serveur des assos qui gère
-  * la connexion permanente (à la demande de l'utilisateur)
-  */
+/* Cette page est appellée lorsque l'utilisateur vient d'autoriser votre apllication sur le serveur des assos
+* Si il refuse d'autoriser votre application il n'arrivera pas sur cette page.
+* Cette page permet de convertir le code renvoyé par oauth2 en access token, qui devra être renseigné pour chaque requête sur le portail
+* Le seul élément à sauvegarder est l'access token. On le sauvegarde dans la session de l'utilisateur et non dans ses cookies car c'est le serveur des assos qui gère
+* la connexion permanente (à la demande de l'utilisateur)
+*/
 
-  require_once("config.php");
+require_once("config.php");
 
-  //Si le state enregistré précedemment est différent ou n'existe pas, soit la session a expiré soit l'utilisateur est malveillant
-  if (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
-      //On détruit le state (on peut tenter de le faire se reconnecter)
-      if (isset($_SESSION['oauth2state'])) {
-          unset($_SESSION['oauth2state']);
-      }
+//Si le state enregistré précedemment est différent ou n'existe pas, soit la session a expiré soit l'utilisateur est malveillant
+if (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
+  //On détruit le state (on peut tenter de le faire se reconnecter)
+  if (isset($_SESSION['oauth2state'])) {
+    unset($_SESSION['oauth2state']);
+  }
 
-      exit('Invalid state');
-  } else {
-      //On essaye de convertir le code retourné par oauth en access_token (qui ne sont à stocker que dans la session)
-      try {
-          // En utilisant la méthode d'autorisation d'oauth2, on génère le token avec le code
-          $accessToken = $provider->getAccessToken('authorization_code', [
-              'code' => $_GET['code']
-          ]);
+  exit('Invalid state');
+} else {
+  //On essaye de convertir le code retourné par oauth en access_token (qui ne sont à stocker que dans la session)
+  try {
+    // En utilisant la méthode d'autorisation d'oauth2, on génère le token avec le code
+    $accessToken = $provider->getAccessToken('authorization_code', [
+      'code' => $_GET['code']
+    ]);
 
-          //echo 'Access Token: ' . $accessToken->getToken() . "<br>";
-          //echo 'Refresh Token: ' . $accessToken->getRefreshToken() . "<br>";
-          //echo 'Expires dans : ' . $accessToken->getExpires() . "<br>";
-          //echo 'Expiré ? ' . ($accessToken->hasExpired() ? 'expiré' : 'non expiré') . "<br>";
+    //echo 'Access Token: ' . $accessToken->getToken() . "<br>";
+    //echo 'Refresh Token: ' . $accessToken->getRefreshToken() . "<br>";
+    //echo 'Expires dans : ' . $accessToken->getExpires() . "<br>";
+    //echo 'Expiré ? ' . ($accessToken->hasExpired() ? 'expiré' : 'non expiré') . "<br>";
 
-          //On sauvegarde l'access token dans la session
-          $_SESSION["access_token"] = $accessToken->jsonSerialize();
+    //On sauvegarde l'access token dans la session
+    $_SESSION["access_token"] = $accessToken->jsonSerialize();
 
-          //On redirige l'utillisateur
-          header('Location: ' . "/oauth2_simde_portail_exemple/PHP_sans_laravel/");
-          exit;
-      } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+    //On redirige l'utillisateur
+    header('Location: ' . "/oauth2_simde_portail_exemple/PHP_sans_laravel/");
+    exit;
+  } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
 
-          //Une erreur a eu lieu pendant la récupération du token.
-          exit($e->getMessage());
-
-      }
+    //Une erreur a eu lieu pendant la récupération du token.
+    exit($e->getMessage());
 
   }
+}
 ?>
