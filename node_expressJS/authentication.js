159a3f7333e95ccc2d8c993a1fbfaf01d8829b70
Victor tondolo <victor.tondolo@gmail.com>
2021-07-19 12:07:43 +0200

    Improve node_example

--- node_expressJS/authentication.js
+++ node_expressJS/authentication.js
@@ -24,13 +24,16 @@ var authURL = oauth2.getAuthorizeUrl({
 });
 
 const authenticationFilter = function (req, res, next) {
-    /**
-     * Check if the request contains a valid token
-     */
-    const token = req.header('authorization');
+    // Check if the request contains a valid token
+    let token = req.header('authorization');
     if (token !== null && token !== undefined && token !== '') {
-        // Check if the token is valid (use routes)
-        axios({
+        // Chek if we receive a Bearer token
+        if (!token.startsWith("Bearer")){
+            token =  'Bearer ' + token;
+        }
+
+        // Check if the token is valid (used route: user's information)
+        const responseAxios = await axios({
             method: 'GET',
             url: `${portailURL}/api/v1/user`,
             headers: {
@@ -38,49 +41,45 @@ const authenticationFilter = function (req, res, next) {
                 'Accept-Charset': 'utf-8',
                 'Authorization': token
             }
-        }).then(function (response) {
-            // The token is not valid
-            if (response.status !== 200) {
-                // Redirect to portail
-                return res.redirect(authURL);
-            }
-        }).catch(function (err) {
-            console.error(err);
+        }).catch((err) => {
+            return err.response;
+        }).then((response) => {
+            return response;
         });
+
+        if ( responseAxios.status !== 200) {
+            return res.redirect(authURL);
+        }
 
         // Send the request to next server's middlware
         next();
         return;
     }
 
-    /**
-     * Check if the request has query parameters named code
-     */
+    // Check if the request has query parameters named code
     const authorizationCode = req.query.code;
 
     if (authorizationCode === null || authorizationCode === undefined || authorizationCode === '') {
         // Handle redirection (the user is not connected with oauth2 yet)
         return res.redirect(authURL);
     } else {
-        /** Obtaining access_token */
+        // Obtaining access_token
         oauth2.getOAuthAccessToken(
             authorizationCode,
             {
                 'redirect_uri': redirectURL,
                 'grant_type':'authorization_code'
             },
-            function (err, access_token, refresh_token, results){
+            async function (err, access_token, refresh_token, results){
                 if (err) {
-                    console.log(err);
-                    res.end(err);
-                    return;
+                    Logger.error(err);
+                    return res.status(500).send('Internal Server Error');
                 } else if (results.error) {
-                    console.log(results);
-                    res.end(JSON.stringify(results));
+                    Logger.error(results);
+                    return res.status(500).send('Internal Server Error');
                 }
                 else {
-                    console.log('Obtained access_token: ', access_token);
-                    res.end(access_token);
+                    Logger.debug(`Obtained access_token:  ${access_token}`);
 
                     // Get information from the connected user
                     axios({
@@ -93,17 +92,16 @@ const authenticationFilter = function (req, res, next) {
                         }
                     }).then(function (response) {
                         // Print user information
-                        console.log(response.data);
+                        Logger.debug(response.data);
+                        next();
                     }).catch(function (err) {
                         console.error(err);
+                        return res.status(500).send('Internal Server Error');
                     });
                 }
             }
         );
     }
-
-    // Send the request to next server's middlware
-    next();
 };
 
 module.exports = authenticationFilter;
